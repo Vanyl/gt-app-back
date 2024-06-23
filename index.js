25bacc8248917591083eaf1e423b4dcbe1295c16
Yavanna Perez <yavannaperez@hotmail.com>
2024-06-23 20:08:28 +0200

    clean code + change status

--- index.js
+++ index.js
@@ -25,12 +25,12 @@ app.post('/api/register', function (req, res) {
     } else {
         Users.filter(function (user) {
             if (user.email === req.body.email) {
-                res.send({message: "User Already Exists! Login or register with another email"});
+                res.status(409).json({ message: "User Already Exists! Login or register with another email" });
             }
         });
         var newUser = { name: req.body.name, email: req.body.email, password: req.body.password };
         Users.push(newUser);
-        res.status(200).json({ message: "User registered successfully!" });
+        res.status(201).json({ message: "User registered successfully! Please login" });
     }
 })
 
@@ -43,31 +43,20 @@ app.post('/api/login', function (req, res) {
     console.log('Received data:', req.body);
     console.log('Users :');
     console.log(Users)
+
     if (!req.body.email || !req.body.password) {
         res.status(400).json({ message: "Please enter both email and password to log in" });
     } else {
-           Users.filter(function(user){
-        if (user.email === req.body.email && user.password === req.body.password) {
-            //  req.session.user = user;
-            //  res.redirect('/protected_page');
-            res.sendStatus(200);
+        const user = Users.find((user) => user.email === req.body.email && user.password === req.body.password);
+
+        if (user) {
+            res.status(200).json({ message: "Login successful", name: user.name });
+        } else {
+            res.status(401).json({ error: "Invalid credentials!" });
         }
-           });
-        res.status(401).json({ message: "Invalid credentials!" });
+
     }
 });
-
-// app.get('/api/logout', function (req, res) {
-//     req.session.destroy(function () {
-//         console.log("user logged out.")
-//     });
-//     res.redirect('/login');
-// });
-
-app.get('/hello', (req, res) => {
-    console.log('aloha')
-    res.send('Hello World!')
-})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
